7725bf956c2da43189c55a2f315ac71f470237e6
PrettyXD <ahmetemre617@gmail.com>
2024-06-26 18:55:31 +0300

    reroll cuz mistakes

--- frontend/popup.js
+++ frontend/popup.js
@@ -1,6 +1,6 @@
 document.getElementById('checkButton').addEventListener('click', async () => {    
     const resultDiv = document.getElementById('result');
-    resultDiv.innerHTML = '<div class="loading-indicator">Loading...</div>'; // Show loading indicator
+    resultDiv.style.display = 'none';
     let [tab] = await chrome.tabs.query({active: true, currentWindow: true});
     if (tab.url) {
         try {
@@ -12,11 +12,12 @@ document.getElementById('checkButton').addEventListener('click', async () => {
                 body: JSON.stringify({url: tab.url}),
             });
             const data = await response.json();
-            resultDiv.innerHTML = `<div class="description-text">Website Description: ${data.result}</div>`; // Use innerHTML for formatting
+            resultDiv.textContent = `Website Description: ${data.result}`;
             resultDiv.classList.remove('alert-danger', 'alert-success'); // Remove specific styling for phishing check
+            resultDiv.style.display = 'block';
         } catch (error) {
             console.error('Error:', error);
-            resultDiv.innerHTML = '<div class="description-text">Error retrieving website description.</div>';
+            document.getElementById('result').textContent = 'Error retrieving website description.';
         }
     } else {
         console.log('No URL found in current tab');
